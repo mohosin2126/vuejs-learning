1efe690df39aed6b4b7e51ce0020291940dce855
Md Mohosin Ali Shah <mohosin2126@gmail.com>
2025-04-13 12:11:03 +0600

    vue js init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref} from "vue";
+const helloworld = ref('');
+helloworld.value = 'Hello World.';
+const visiable = ref(true);
+</script>
+
+<template>
+  <div v-if="visiable">
+    <h1 class="text-red-500">{{helloworld}}</h1>
+  </div>
+  <div v-else>
+    <h1 class="text-red-500">Hello world from not visible</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
